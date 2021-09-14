6e06da74b1fdcfae9b395a08524e389b79f75f86
Christopher Boyle <cboyle824@gmail.com>
2021-09-14 12:37:58 -0400

    code cleanup

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET } = require("../secrets");
 const db = require("./../../data/db-config");
 
 const restricted = (req, res, next) => {
@@ -23,21 +23,6 @@ const restricted = (req, res, next) => {
       message: "Token required",
     });
   }
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
 };
 
 const only = (role_name) => (req, res, next) => {
@@ -49,16 +34,6 @@ const only = (role_name) => (req, res, next) => {
       message: "This is not for you",
     });
   }
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": "This is not for you"
-    }
-
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
 };
 
 const checkUsernameExists = async (req, res, next) => {
@@ -73,13 +48,6 @@ const checkUsernameExists = async (req, res, next) => {
   } else {
     next();
   }
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": "Invalid credentials"
-    }
-  */
 };
 
 const validateRoleName = (req, res, next) => {
@@ -102,24 +70,6 @@ const validateRoleName = (req, res, next) => {
     req.body.role_name = role_name.trim();
     next();
   }
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
-
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
 };
 
 module.exports = {
--- api/auth/token-builder.js
+++ api/auth/token-builder.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../secrets/index");
+const { JWT_SECRET } = require("../secrets");
 
 const buildToken = (user) => {
   const payload = {
